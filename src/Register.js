9a7e920c0e5e2a1e7fe76a6d08ae0b4cee0eca07
ARUNREACT <convey2arunk@gmail.com>
2023-10-11 01:58:40 +0530

    Updated API endpoints to use Elastic Beanstalk domain

--- src/Register.js
+++ src/Register.js
@@ -14,7 +14,7 @@ function Register()
 	{
 		let item={name,email,password};
 		console.warn(item);
-		let result= await fetch("http://localhost:8000/api/register",{
+		let result= await fetch("http://day-env.eba-ffubwman.ap-southeast-1.elasticbeanstalk.com/api/register",{
 			method:'POST',
 			body:JSON.stringify(item),
 			headers:{
